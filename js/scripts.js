056d2c211939d1b171d8702574beba053a7f3949
Roger Tobias <celticspyder14@gmail.com>
2017-05-02 15:07:44 -0700

    This commit wiil add the test for the count by number to be greater to the count to number, and the for loop for counting up the numbers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("#userNumbers").submit(function(event){
+  $("#userNumbers").submit(function(event) {
     debugger;
     var countTo = $("input#countTo").val();
     var countBy = $("input#countBy").val();
@@ -11,8 +11,15 @@ $(document).ready(function() {
       alert("Numbers only & no empty fields.");
     } else if ( countTo < 0 || countBy < 0) {
       alert("Both fields need to be greater than 0.")
+    } else if (countTo < countBy) {
+      alert("You're counting by a number greater than you're counting to.")
     } else {
-      
+      var displayNum = 0;
+      for (var i = 0; i < countTo; i++) {
+        displayNum += countBy;
+      }
+
+
     }
 
     event.preventDefault();
